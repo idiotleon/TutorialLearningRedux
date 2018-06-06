c0c8066b3c91e99bab140f8bc8b0e05fa722875f
Leon Lu <yanglu@microsoft.com>
2018-06-06 14:30:42 -0700

    demo: Add an error
    module 2.3: Create Array Reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -9,3 +9,15 @@ export const skiDay = (state = null, action) =>
     (action.type === C.ADD_DAY) ?
         action.payload :
         state
+
+export const errors = (state = [], action) => {
+    switch (action.type) {
+        case C.ADD_ERROR:
+            return [
+                ...state,
+                action.payload
+            ]
+        default:
+            return state
+    }
+}
